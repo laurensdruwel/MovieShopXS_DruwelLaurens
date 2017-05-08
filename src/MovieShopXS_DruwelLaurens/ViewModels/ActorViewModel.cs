559497e7ae3e1ecdf943a641ef503710ff0da0c4
laurensdruwel <laurens.druwel@student.howest.be>
2017-05-08 10:39:54 +0200

    Add project files.

---
+++ src/MovieShopXS_DruwelLaurens/ViewModels/ActorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopXS_DruwelLaurens.ViewModels
+{
+    public class ActorViewModel
+    {
+    }
+}
---
+++ src/MovieShopXS_DruwelLaurens/ViewModels/MovieViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopXS_DruwelLaurens.ViewModels
+{
+    public class MovieViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public int Director { get; set; }
+        public int Stars { get; set; }
+        public ICollection<ActorViewModel> Actors { get; set; }
+
+    }
+}
